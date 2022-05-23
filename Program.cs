a8c10e31dbf3b3886a93294408c1776f4008dac2
akshay-ugale <ugaleakshay07@gmail.com>
2022-05-23 12:10:12 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Google_Interview_questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
